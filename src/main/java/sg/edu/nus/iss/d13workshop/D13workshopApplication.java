8986342678be4c88155c777a80b3a9682f74b759
partytaitai <partytaitai@MacBook-Pro-3.local>
2022-07-20 10:04:24 +0800

    first commit

---
+++ src/main/java/sg/edu/nus/iss/d13workshop/D13workshopApplication.java
@@ -0,0 +1,15 @@
+package sg.edu.nus.iss.d13workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D13workshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D13workshopApplication.class, args);
+
+		
+	}
+
+}
